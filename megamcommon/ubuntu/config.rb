f1393f4abd72ec217b4f69658f9422b8209420fb
Thomas Alrin <alrin@megam.co.in>
2014-07-08 11:18:57 +0530

    config changes

--- megamcommon/ubuntu/config.rb
+++ megamcommon/ubuntu/config.rb
@@ -1,12 +1,6 @@
 module MegamPkg
   class Ger
   CONFIG =  {:os => ['trusty', 'precise'],
-        :name => "megamnilavu"}
-
-  if !Gem::Specification::find_all_by_name('pkgr').any?
-      puts "gem install pkgr"
-      system 'gem install pkgr'
-  end
-
+        :name => "megamcommon"}
   end
 end
