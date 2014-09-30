1af406299257ee686b94b080ba22952bdb3cd8ef
Kishorekumar Neelamegam <nkishore@megam.co.in>
2014-10-01 02:13:09 +0530

    A new package for megamchefnative

---
+++ megamchefnative/ubuntu/config.rb
@@ -0,0 +1,7 @@
+module MegamPkg
+  class Ger
+  CONFIG =  {:os => ['trusty', 'precise'],
+        :name => "megamchefnative",
+        :git => "https://github.com/ctdk/goiardi"}
+  end
+end
--- megamnilavu/ubuntu/config.rb
+++ megamnilavu/ubuntu/config.rb
@@ -1,6 +1,7 @@
 module MegamPkg
   class Ger
   CONFIG =  {:os => ['trusty', 'precise'],
+	:branch => "0.5",
         :name => "megamnilavu"}
 
   if !Gem::Specification::find_all_by_name('pkgr').any?
