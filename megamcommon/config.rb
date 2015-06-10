9f83743c3baa2209291ba4bc60feaa5837b18f10
indykish <nkishore@megam.io>
2015-06-10 18:16:36 +0530

    changes for megamcommon and meg

--- megamcommon/config.rb
+++ megamcommon/config.rb
@@ -1,7 +1,7 @@
 module MegamPkg
   class Ger
-    attr_writer :halwa, :build_halwa
-    CONFIG =  { os: %w(trusty precise jessie centos7), branch: '0.8', name: 'megamnilavu' }
+    attr_accessor :halwa, :build_halwa
+    CONFIG =  { os: %w(trusty precise jessie centos7), branch: '0.8', name: 'megamcommon' }
 
     def os_ok!(build_os)
       raise "--- You have two horns. \nUnsupported os: #{build_os}" unless CONFIG[:os].include? build_os
