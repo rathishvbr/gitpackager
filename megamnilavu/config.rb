ecd9419f9fc0b9cb600dd8920680f7f98ea87685
indykish <nkishore@megam.io>
2015-06-11 13:25:20 +0530

    minor tweaks to reference megger.halwa and megger.build_halwa

--- megamnilavu/config.rb
+++ megamnilavu/config.rb
@@ -3,7 +3,7 @@ module MegamPkg
     attr_accessor :halwa, :build_halwa
     CONFIG =  { os: %w(trusty precise jessie centos7), branch: '0.8', name: 'megamnilavu' }
 
-    if !Gem::Specification::find_all_by_name('pkgr').any?
+    imgaf !Gem::Specification::find_all_by_name('pkgr').any?
           puts "gem install pkgr"
           system 'gem install pkgr'
     end
