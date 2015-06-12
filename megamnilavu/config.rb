f14d46a3de74e85dcbce6f36e51820abee6997c9
Yeshwanth kumar <getyesh@megam.io>
2015-06-12 21:04:48 +0530

    some build passes! - changed clone url

--- megamnilavu/config.rb
+++ megamnilavu/config.rb
@@ -3,10 +3,10 @@ module MegamPkg
     attr_accessor :halwa, :build_halwa
     CONFIG =  { os: %w(trusty precise jessie centos7), branch: '0.8', name: 'megamnilavu' }
 
-    imgaf !Gem::Specification::find_all_by_name('pkgr').any?
-          puts "gem install pkgr"
-          system 'gem install pkgr'
-    end
+#    imgaf !Gem::Specification::find_all_by_name('pkgr').any?
+ #         puts "gem install pkgr"
+ #         system 'gem install pkgr'
+    #end
 
     def os_ok!(build_os)
       raise "--- You have two horns. \nUnsupported os: #{build_os}" unless CONFIG[:os].include? build_os
