682bddcfd85aad9130f6d611ef2fb1e1d91a7cc3
Yeshwanth kumar <getyesh@megam.io>
2015-06-16 13:20:38 +0530

    fixes

---
+++ megamanalytics/config.rb
@@ -0,0 +1,16 @@
+
+module MegamPkg
+  class Ger
+    attr_accessor :halwa, :build_halwa
+    CONFIG =  { os: %w(trusty precise jessie centos7), branch: '0.8', tap_name: 'megamanalytics', heka_name: "megamheka" }
+
+if !Gem::Specification::find_all_by_name('pkgr').any?
+      puts "gem install pkgr"
+      system 'gem install pkgr'
+  end
+
+    def os_ok!(build_os)
+      raise "--- You have two horns. \nUnsupported os: #{build_os}" unless CONFIG[:os].include? build_os
+    end
+  end
+end
--- megamgulpd/config.rb
+++ megamgulpd/config.rb
@@ -1,7 +1,7 @@
 module MegamPkg
   class Ger
     attr_accessor :halwa, :build_halwa
-    CONFIG =  { os: %w(trusty precise jessie centos7), branch: '0.8', name: 'megamgulpd' }
+    CONFIG =  { os: %w(trusty precise jessie centos7), branch: '0.8', name: 'megamgulpd', :git => "https://github.com/megamsys/gulp" }
 
     def os_ok!(build_os)
       raise "--- You have two horns. \nUnsupported os: #{build_os}" unless CONFIG[:os].include? build_os
