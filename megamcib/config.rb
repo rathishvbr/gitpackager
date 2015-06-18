79ec6acd9a650b3107f36a1824a9ff70f9a18fa7
anandh02 <anandhperumal02@gmail.com>
2015-06-18 21:18:48 +0530

    build packages

--- megamcib/config.rb
+++ megamcib/config.rb
@@ -1,7 +1,7 @@
 module MegamPkg
   class Ger
     attr_accessor :halwa, :build_halwa
-    CONFIG =  { os: %w(trusty precise jessie centos7), branch: '0.8', name: 'megamcib' }
+    CONFIG =  { os: %w(trusty precise jessie centos7), branch: 'master', name: 'megamcib', :git => "https://github.com/megamsys/cloudinabox.git"}
 
     def os_ok!(build_os)
       raise "--- You have two horns. \nUnsupported os: #{build_os}" unless CONFIG[:os].include? build_os
