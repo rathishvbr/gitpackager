bd15f1c36e9b8148c3582c36fc29a9118299d063
vijaykanthm28 <mvijaykanth@megam.io>
2015-11-05 11:14:36 +0530

    aptly apt-get url changed

--- megamcadvisor/config.rb
+++ megamcadvisor/config.rb
@@ -1,10 +1,11 @@
 module MegamPkg
   class Ger
     attr_accessor :halwa, :build_halwa
-    CONFIG =  { os: %w(trusty precise jessie centos7), branch: 'master', name: 'cadvisor', :git => "https://github.com/megamsys/cadvisor.git" }
+    CONFIG =  { os: %w(trusty precise jessie centos7), branch: 'master', name: 'megamcadvisor', :git => "https://github.com/megamsys/cadvisor.git" }
 
     def os_ok!(build_os)
       raise "--- You have two horns. \nUnsupported os: #{build_os}" unless CONFIG[:os].include? build_os
     end
   end
 end
+
