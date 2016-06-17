6b466c6da57a03d0c09d1f08f7b00f53acc41593
rajesh-rajagopal <rajeshr@megam.io>
2016-06-17 20:34:26 +0530

    Add reprepro shell script

--- verticecommon/config.rb
+++ verticecommon/config.rb
@@ -1,7 +1,7 @@
 module MegamPkg
   class Ger
     attr_accessor :halwa, :build_halwa
-    CONFIG =  { os: %w(trusty jessie centos7), branch: '0.9', name: 'verticecommon' }
+    CONFIG =  { os: %w(trusty jessie centos7), branch: '1.0', name: 'verticecommon' }
 
     def os_ok!(build_os)
       raise "--- You have two horns. \nUnsupported os: #{build_os}" unless CONFIG[:os].include? build_os
