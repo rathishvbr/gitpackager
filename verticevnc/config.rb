eaafebab6caa2d25fad3759be26668316c1f6c67
rajesh-rajagopal <rajeshr@megam.io>
2016-07-29 15:43:57 +0530

    debian package creation changes

--- verticevnc/config.rb
+++ verticevnc/config.rb
@@ -3,11 +3,6 @@ module MegamPkg
     attr_accessor :halwa, :build_halwa
     CONFIG =  { os: %w(trusty jessie centos7), branch: 'master', name: 'verticevnc' }
 
-    #    imgaf !Gem::Specification::find_all_by_name('pkgr').any?
-    #         puts "gem install pkgr"
-    #         system 'gem install pkgr'
-    #end
-
     def os_ok!(build_os)
       raise "--- You have two horns. \nUnsupported os: #{build_os}" unless CONFIG[:os].include? build_os
     end
