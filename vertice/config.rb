cac8d5f35faf3e8d6208fbf4130202f917181f07
rajesh-rajagopal <rajeshr@megam.io>
2016-08-23 13:22:44 +0530

    jessie & centos build packages changes

--- vertice/config.rb
+++ vertice/config.rb
@@ -1,7 +1,7 @@
 module MegamPkg
   class Ger
     attr_accessor :halwa, :build_halwa
-    CONFIG =  { os: %w(trusty jessie centos7), branch: 'master', name: 'vertice', :git => "https://github.com/megamsys/vertice.git" }
+    CONFIG =  { os: %w(trusty jessie centos7), branch: '1.5', name: 'vertice', :git => "https://github.com/megamsys/vertice.git" }
 
     def os_ok!(build_os)
       raise "--- You have two horns. \nUnsupported os: #{build_os}" unless CONFIG[:os].include? build_os
