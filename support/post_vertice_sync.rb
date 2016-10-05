c05a851f7126717b8c40eaccc9268494d7a64209
vijaykanthm28 <mvijaykanth@megam.io>
2016-10-05 19:35:17 +0530

    file rename and remove unused codes

--- support/post_vertice_sync.rb
+++ support/post_vertice_sync.rb
@@ -3,13 +3,9 @@
 ONE_LOCATION=ENV["ONE_LOCATION"]
 
 if !ONE_LOCATION
-    RUBY_LIB_LOCATION="/usr/lib/one/ruby"
-    VMDIR="/var/lib/one"
     CONFIG_FILE="/var/lib/megam/master_key"
     LOG_FILE="/var/log/one/host_error.log"
 else
-    RUBY_LIB_LOCATION=ONE_LOCATION+"/lib/ruby"
-    VMDIR=ONE_LOCATION+"/var"
     CONFIG_FILE=ONE_LOCATION+"/var/config"
     LOG_FILE=ONE_LOCATION+"/var/host_error.log"
 end
@@ -59,45 +55,25 @@ end
 
 ###############################################################################
 
+TEMPLATE = ARGV[0]
 
-TEMPLATE = ARGV[1]
+if TEMPLATE.nil?
+    log_error("Exiting due to Template is  nil.")
+    exit -1
+end
+
 content = TEMPLATE
 s  = Base64.decode64(content.inspect)
 
 KEYS = Hash[*File.read(CONFIG_FILE).split(/[= \n]+/)]
-STATE  = ARGV[2]
 
-HOST_ID = ARGV[0]
-
-if HOST_ID.nil?
-    exit -1
-end
+STATE  = ARGV[1]
+STATUS = ARGV[2]
 
 @doc  = Nokogiri::XML(s) 
 
-
-case @doc.xpath("//STATE").text
-when "3"
-  case @doc.xpath("//LCM_STATE").text
-  when "18"
-  status = "power_off"
-  state = "stopped"
-  when "21"
-  status = "SUSPENDED"
-  state = "hold"
-  else
-    status = "unknown"
-    state = "unknown"
-#  we have to add for all lcm_states
-  end
-when "6"
-   status = "deleting"
-   state = "remove"
-end
-
-KEYS["status"] = status
-KEYS["state"] = state
-
+KEYS["status"] = STATUS
+KEYS["state"] = STATE
 KEYS[:email]   = @doc.xpath("//ACCOUNTS_ID").text
 KEYS["id"]      = @doc.xpath("//ASSEMBLY_ID").text
 KEYS["asms_id"] = @doc.xpath("//ASSEMBLIES_ID").text
@@ -118,8 +94,8 @@ KEYS["org_id"] = @doc.xpath("//ORG_ID").text  #//gateway has change to get from 
 begin
    m  = Megam::Assembly.update(KEYS)
 rescue Exception => e
-log_error e.to_s
-exit -1
+  log_error e.to_s
+  exit -1
 end
 
 log "Hook finished:"
